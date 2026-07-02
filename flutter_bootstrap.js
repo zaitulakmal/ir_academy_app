d0ecef8b31d288b4f8a0830cc0876751180b8a2a
Zaitul Akmal <94424179+zaitulakmal@users.noreply.github.com>
2026-07-02 12:19:57 +0800

    Fix Firebase init: move to _load(), add apps.isEmpty guard

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1354007081" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3558269185" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
