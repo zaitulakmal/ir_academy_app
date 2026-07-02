72eea4ed3ebd5c83c265933fcb1bcf8fc6a21c3c
Zaitul Akmal <94424179+zaitulakmal@users.noreply.github.com>
2026-07-02 11:45:02 +0800

    Add error display and user count for debugging

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1912671189" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4129729281" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
