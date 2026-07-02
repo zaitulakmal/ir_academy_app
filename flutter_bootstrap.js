63ad144c62b7f3c04b9d3632c4497ec095823320
Zaitul Akmal <94424179+zaitulakmal@users.noreply.github.com>
2026-07-02 11:39:53 +0800

    Fix login: ilike partial match + autocomplete optionsBuilder

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2620441760" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1912671189" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
