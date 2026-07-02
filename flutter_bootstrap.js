86bd393ab960fdde725c34291dd7ec76943e6b59
Zaitul Akmal <94424179+zaitulakmal@users.noreply.github.com>
2026-07-02 10:55:58 +0800

    Replace student chip grid with searchable multi-select dialog

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3307045002" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "924466212" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
